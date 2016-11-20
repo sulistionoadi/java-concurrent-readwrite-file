bbe7cb6fd16aeb1535d7d3bcc68899ce0163843e
Adi Sulistiono <sulistiono.adi@gmail.com>
2016-11-21 00:06:28 +0700

    membuat configurasi ThreadPoolTaskExecutor

--- src/main/java/com/sulistionoadi/belajar/JavaConcurrentReadwriteFileApplication.java
+++ src/main/java/com/sulistionoadi/belajar/JavaConcurrentReadwriteFileApplication.java
@@ -2,12 +2,24 @@ package com.sulistionoadi.belajar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 public class JavaConcurrentReadwriteFileApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaConcurrentReadwriteFileApplication.class, args);
+    }
+    
+    @Bean(name="processingThreadPool")
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+        pool.setCorePoolSize(0);
+        pool.setMaxPoolSize(200);
+        pool.setWaitForTasksToCompleteOnShutdown(true);
+        pool.setQueueCapacity(500);
+        return pool;
     }
     
 }
